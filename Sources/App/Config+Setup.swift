17e11cbab3631ec913cc3f0a3cbbf432686a26ad
Keith Moon <keef@keefmoon.com>
2018-11-19 23:32:54 +0000

    Updating Vapor version

--- Sources/App/Config+Setup.swift
+++
@@ -1,8 +0,0 @@
-
-extension Config {
-    public func setup() throws {
-        // allow fuzzy conversions for these types
-        // (add your own types here)
-        Node.fuzzy = [JSON.self, Node.self]
-    }
-}
